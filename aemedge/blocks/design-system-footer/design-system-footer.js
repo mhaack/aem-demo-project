a7fba5fcd55942215477a385098e5d002817bf3a
Eduard Seifert <eduard.seifert@sap.com>
2024-05-08 15:38:28 +0200

    629 design system footer (#729)
    * #629 feat: first draft for DS footer
    
    * #600 feat: Design System TOC (On this page) WIP
    
    * #629 feat: footer styling and viewport behaviour
    
    * #629 adjusted logo link and divider appearance
    
    * #629 removed console.log statement
    
    * #629 fix: reverted merge deletions

--- aemedge/blocks/design-system-footer/design-system-footer.js
+++ aemedge/blocks/design-system-footer/design-system-footer.js
@@ -1,7 +1,18 @@
-import decorateFooter from '../footer/footer.js';
+import { decorateIcons, getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  console.log('decorating design system footer');
   block.classList.add('footer');
-  decorateFooter(block);
+
+  const footerMeta = getMetadata('footer');
+  block.textContent = '';
+
+  const footerPath = footerMeta.footer || '/fiori-design-web/footer';
+  const fragment = await loadFragment(footerPath);
+
+  const footer = document.createElement('div');
+  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  decorateIcons(footer);
+  block.append(footer);
 }
