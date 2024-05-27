69d2a7f65e7947e485196dec241c1352f1b7a7cb
Patrick Hupe <hupe@adobe.com>
2024-05-27 12:39:41 +0200

    fix: add analytics (single launch container, slc) for design-system. Currently, every user is tracked. (#890)
    todo: make getScope() return null if tracking shall be disabled (= for internal users)

--- aemedge/scripts/delayed.js
+++ aemedge/scripts/delayed.js
@@ -16,9 +16,14 @@ function getEnvType(hostname = window.location.hostname) {
 }
 
 function getScope(path = window.location.pathname) {
-  return path.startsWith('/news/')
-    || (path.startsWith('/blogs/') && getMetadata('article:tag')?.includes('content-type/executive-blog'))
-    ? 'mvp' : 'full';
+  if (path.startsWith('/news/')
+    || (path.startsWith('/blogs/') && getMetadata('article:tag')?.includes('content-type/executive-blog'))) {
+    return 'ch_mvp_slc';
+  }
+  if (path.startsWith('/design/') || path.startsWith('/design-system/')) {
+    return 'ds_slc'; // return null if internal
+  }
+  return 'ch_full_slc';
 }
 
 async function sendBeacon(stl = null) {
@@ -30,15 +35,20 @@ async function sendBeacon(stl = null) {
 async function loadAdobeDC() {
   const prefix = 'https://assets.adobedtm.com/ccc66c06b30b';
   const adobeTagsSrc = {
-    mvp: {
+    ch_mvp_slc: {
       dev: `${prefix}/6fa889b263e0/launch-3318725e3375-development.min.js`,
       stage: `${prefix}/6fa889b263e0/launch-6615f1bb001d-staging.min.js`,
       prod: `${prefix}/6fa889b263e0/launch-69ede165613b.min.js`,
     },
-    full: {
+    ch_full_slc: {
       dev: `${prefix}/25f24eee2abd/launch-f3136a1cc085-development.min.js`,
       stage: `${prefix}/25f24eee2abd/launch-6f23898368f1-staging.min.js`,
       prod: `${prefix}/25f24eee2abd/launch-2fd2742fb9c4.min.js`,
+    },
+    ds_slc: {
+      dev: `${prefix}/57f4f8e3a781/launch-92f6f7a945ee-development.min.js`,
+      stage: `${prefix}/57f4f8e3a781/launch-131489947aa0-staging.min.js`,
+      prod: `${prefix}/57f4f8e3a781/launch-b3931adab11c.min.js`,
     },
   };
   const envType = getEnvType();
