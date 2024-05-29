0d5456477a5cccccd6e2f9bd1ea5ec1e1c06c91f
Eduard Seifert <eduard.seifert@sap.com>
2024-05-29 11:11:17 +0200

    770 ds colors page (#907)
    * #770 feat: Cards variant colors WIP
    
    * #770 feat: flexbox for cards, grid adjustments
    
    * #770 feat: added eyebrow and title classes, restructured JS a bit
    
    * #770 feat: caching reference

--- aemedge/blocks/cards/cards.js
+++ aemedge/blocks/cards/cards.js
@@ -1,4 +1,83 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import {
+  div,
+  domEl,
+} from '../../scripts/dom-builder.js';
+
+/**
+ * Decorate Cards (colors) variant.
+ * Add color preview div to each card and manipulate color code elements.
+ * @param block {HTMLElement}
+ */
+function decorateColorsVariant(block) {
+  if (!block.classList.contains('colors')) { return; }
+
+  const cards = block.querySelectorAll('.cards-card');
+
+  /**
+   * Label eyebrow and title based on the number of paragraphs.
+   * @param cardBody {HTMLElement}
+   */
+  function labelEyebrowAndTitle(cardBody) {
+    const paragraphs = cardBody.querySelectorAll('p');
+    if (paragraphs.length > 3) {
+      cardBody.firstElementChild?.classList.add('cards-card-color-eyebrow');
+      cardBody.children[1]?.classList.add('cards-card-color-title');
+    } else {
+      cardBody.firstElementChild?.classList.add('cards-card-color-title');
+    }
+  }
+
+  /**
+   * Wrap color code in `code` tag and add class to the color element.
+   * @param paragraphs {NodeList}
+   */
+  function wrapColorCode(paragraphs) {
+    paragraphs.forEach((p) => {
+      const text = p.textContent;
+      if (text.startsWith('#')) {
+        p.classList.add('cards-card-color-hex');
+      } else if (text.startsWith('rgb')) {
+        p.classList.add('cards-card-color-rgb');
+      } else {
+        return;
+      }
+
+      const code = domEl('code', text.toUpperCase());
+      p.textContent = '';
+      p.append(code);
+    });
+  }
+
+  /**
+   * Add color preview div to each card.
+   * @param colorsHex {*[]}
+   */
+  function addColorPreview(colorsHex) {
+    colorsHex.forEach((color) => {
+      const bgColor = color.textContent;
+      const isWhite = bgColor === '#FFFFFF';
+      const card = color.closest('.cards-card');
+      const colorPreview = div(
+        {
+          class: `cards-card-color-preview${isWhite ? ' --white' : ''}`,
+          style: `background-color: ${bgColor}`,
+        },
+      );
+      card.prepend(colorPreview);
+    });
+  }
+
+  cards.forEach((card) => {
+    const cardBody = card.querySelector('.cards-card-body');
+    const paragraphs = cardBody.querySelectorAll('p');
+    const colorsHex = [...paragraphs].filter((p) => p.textContent.startsWith('#'));
+
+    labelEyebrowAndTitle(cardBody);
+    wrapColorCode(paragraphs);
+    addColorPreview(colorsHex);
+  });
+}
 
 export default function decorate(block) {
   /* change to ul, li */
@@ -7,9 +86,12 @@ export default function decorate(block) {
     const cardDiv = document.createElement('div');
     cardDiv.className = 'cards-card';
     while (row.firstElementChild) cardDiv.append(row.firstElementChild);
-    [...cardDiv.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+    [...cardDiv.children].forEach((childDiv) => {
+      if (childDiv.children.length === 1 && childDiv.querySelector('picture')) {
+        childDiv.className = 'cards-card-image';
+      } else {
+        childDiv.className = 'cards-card-body';
+      }
     });
     const li = document.createElement('li');
     li.append(cardDiv);
@@ -43,11 +125,14 @@ export default function decorate(block) {
     }
     ul.append(li);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture')
+    .replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.classList.add(`elems${ul.children.length}`);
   if (ul.children.length > 8) {
     block.classList.add('elems9plus');
   }
   block.append(ul);
+
+  decorateColorsVariant(block);
 }
