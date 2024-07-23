c8b921224c4d844ac472a8574100a4dcc5ce64d4
Antonio Marco Simone Bianchetti <gro40402@adobe.com>
2024-07-23 12:41:49 +0200

    1463 Added missing aria label to footer social links (#1464)

--- aemedge/blocks/social-profile/social-profile.js
+++ aemedge/blocks/social-profile/social-profile.js
@@ -2,6 +2,7 @@ export default function decorate(block) {
   block.querySelectorAll('a').forEach((link) => {
     link.setAttribute('target', '_blank');
     link.setAttribute('rel', 'noopener noreferrer');
+    link.setAttribute('aria-label', link.querySelector('[data-icon-name]')?.getAttribute('data-icon-name'));
 
     if (link.href.indexOf('mailto:') === 0) {
       link.setAttribute('target', '_self');
