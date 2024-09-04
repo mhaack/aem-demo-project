b3ea41fcc32b9c23324f862fe2f870dd9c8c3f44
Markus Haack <mhaack@adobe.com>
2024-09-04 14:24:31 +0200

    chore: adjust promo block

--- aemedge/blocks/promo/promo.js
+++ aemedge/blocks/promo/promo.js
@@ -1,5 +1,14 @@
 import { decorateButtons } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
+
+  if (block.childElementCount === 2) {
+    const img = block.firstElementChild.querySelector('img');
+    if (img) {
+      block.lastElementChild.firstElementChild.before(img.closest('div'));
+      block.firstElementChild.remove();
+    }
+  }
+
   decorateButtons(block);
 }
