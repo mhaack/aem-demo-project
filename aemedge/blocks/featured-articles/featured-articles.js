4586916a470c9cb6925598aeebd558961295b9d7
Markus Haack <mhaack@adobe.com>
2024-09-05 17:09:57 +0200

    chore: fix featured articles

--- aemedge/blocks/featured-articles/featured-articles.js
+++ aemedge/blocks/featured-articles/featured-articles.js
@@ -80,7 +80,16 @@ function getPictureCard(article, placeholders, tags, author, eager) {
 
 export default async function decorateBlock(block) {
   const horizontal = block.classList.contains('horizontal');
-  const links = Array.from(block.querySelectorAll('a')).map((link) => new URL(link.href).pathname);
+  const links = Array.from(block.querySelectorAll('a')).map((link) => new URL(link.href).pathname).map((path) => {
+    let p = path;
+    if (p.endsWith('.html')) {
+      p = p.slice(0, -5);
+    }
+    if (p.startsWith('/content/sap')) {
+      p = p.slice(12);
+    }
+    return p;
+  })
   if (links.length > 0) {
     const articles = await fetchPages(links);
     articles.sort(
